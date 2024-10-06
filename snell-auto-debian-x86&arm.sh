9530cd195ae11794d21305ccbb5b023d417eb358
kenny53 <yiai890@hotmail.com>
2024-10-06 13:15:45 +0800

    Update snell-auto-debian-x86&arm.sh

--- snell-auto-debian-x86&arm.sh
+++ snell-auto-debian-x86&arm.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# 定义最新的 Snell 服务器版本号
+latest_version="v4.1.1"
+latest_url_x86="https://dl.nssurge.com/snell/snell-server-$latest_version-linux-amd64.zip"
+latest_url_arm="https://dl.nssurge.com/snell/snell-server-$latest_version-linux-armv7l.zip"
+
 # 检测并安装sudo
 if ! dpkg -s sudo > /dev/null 2>&1; then
     echo "sudo is not installed. Installing sudo..."
@@ -95,32 +100,39 @@ fi
 
 # 检测是否已安装Snell服务器
 if command -v snell-server > /dev/null 2>&1; then
-    # 读取并显示本机IPv4地址
-    ipv4_address=$(curl -4 ip.sb)
-    echo "Snell server is already installed. Updating /etc/snell-server.conf with local IPv4 address ($ipv4_address)..."
+    current_version=$(snell-server --version | grep -oP "v\d+\.\d+\.\d+")
     
-    # 更新 /etc/snell-server.conf 文件内容
-    sudo sed -i "s/0.0.0.0/$ipv4_address/g" /etc/snell-server.conf
+    if [ "$current_version" = "$latest_version" ]; then
+        ipv4_address=$(curl -4 ip.sb)
+        echo "Snell server $current_version is already installed. Updating /etc/snell-server.conf with local IPv4 address ($ipv4_address)..."
+        
+        # 更新 /etc/snell-server.conf 文件内容
+        sudo sed -i "s/0.0.0.0/$ipv4_address/g" /etc/snell-server.conf
 
-    # 显示 /etc/snell-server.conf 文件内容
-    echo -e "\033[31m\033[1mShowing updated /etc/snell-server.conf contents:\033[0m"
-    cat /etc/snell-server.conf
-    
-    exit 0
+        # 显示 /etc/snell-server.conf 文件内容
+        echo -e "\033[31m\033[1mShowing updated /etc/snell-server.conf contents:\033[0m"
+        cat /etc/snell-server.conf
+        
+        exit 0
+    else
+        echo "Current Snell version ($current_version) is outdated. Removing and installing version $latest_version..."
+        sudo systemctl stop snell
+        sudo rm /usr/local/bin/snell-server
+    fi
 fi
 
-# 下载并配置Snell服务器
+# 检测系统架构并下载最新版本的 Snell 服务器
 arch=$(uname -m)
 if [ "$arch" = "x86_64" ]; then
-    snell_url="https://dl.nssurge.com/snell/snell-server-v4.1.1-linux-amd64.zip"
+    snell_url=$latest_url_x86
 elif [[ "$arch" == arm* ]]; then
-    snell_url="https://dl.nssurge.com/snell/snell-server-v4.1.1-linux-armv7l.zip"
+    snell_url=$latest_url_arm
 else
     echo "Unsupported architecture: $arch"
     exit 1
 fi
 
-echo "Downloading and configuring Snell server for architecture: $arch..."
+echo "Downloading and configuring Snell server version $latest_version for architecture: $arch..."
 wget $snell_url
 unzip snell-server-*.zip -d /usr/local/bin
 yes | /usr/local/bin/snell-server --wizard -c /etc/snell-server.conf
